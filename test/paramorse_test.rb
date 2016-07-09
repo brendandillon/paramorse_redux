e9de9263497a946b48fe7e0ed1af1ec1a62627eb
Raphael Barbo <rsbarbo@gmail.com>
2016-07-08 23:15:02 -0600

    updated indentation

--- test/paramorse_test.rb
+++ test/paramorse_test.rb
@@ -7,37 +7,37 @@ require "./lib/paramorse"
 class ParaMorseTest < Minitest::Test
 
 
-def test_queue_pushes_a_word_in
-  q = ParaMorse::Queue.new
-  q.push("something")
-  assert_equal "something", q.queue.join
-end
+  def test_queue_pushes_a_word_in
+    q = ParaMorse::Queue.new
+    q.push("something")
+    assert_equal "something", q.queue.join
+  end
 
-def test_queue_count_one_word
-  q = ParaMorse::Queue.new
-  q.push("something")
-  assert_equal 1, q.queue.count
-end
+  def test_queue_count_one_word
+    q = ParaMorse::Queue.new
+    q.push("something")
+    assert_equal 1, q.queue.count
+  end
 
-def test_queue_pops_a_word_out
-  q = ParaMorse::Queue.new
-  q.push("something")
-  assert_equal "something", q.queue.join
-  q.pop
-  assert_equal [], q.queue
-end
+  def test_queue_pops_a_word_out
+    q = ParaMorse::Queue.new
+    q.push("something")
+    assert_equal "something", q.queue.join
+    q.pop
+    assert_equal [], q.queue
+  end
 
-def test_peek_one_letter
-  q = ParaMorse::Queue.new
-  q.push("something")
-  assert_equal "something", q.queue.join
-  q.pop
-  assert_equal [], q.queue
-end
+  def test_peek_one_letter
+    q = ParaMorse::Queue.new
+    q.push("something")
+    assert_equal "something", q.queue.join
+    q.pop
+    assert_equal [], q.queue
+  end
 
-def test_encode_one_letter
-  letter_encoder = ParaMorse::LetterEncoder.new
-  assert_equal "10111", letter_encoder.encode("a")
-end
+  def test_encode_one_letter
+    letter_encoder = ParaMorse::LetterEncoder.new
+    assert_equal "10111", letter_encoder.encode("a")
+  end
 
 end
