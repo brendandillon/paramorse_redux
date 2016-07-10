ae63e8d4ab4ec73474a4dd4b10cc35fee0f11355
Raphael Barbo <rsbarbo@gmail.com>
2016-07-10 12:00:39 -0600

    removed currently unnecessary files :octocat:

--- lib/file_reader.rb
+++
@@ -1,7 +0,0 @@
-class FileReader
-
-  def self.read(input_read)
-    File.read(input_read).chomp
-  end
-
-end
--- lib/file_translator.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require_relative "file_reader"
-require_relative "paramorse"
-
-  input   = FileReader.read(ARGV[0])
-  file    = ParaMorse.new
-  morse = file.encode_to_morse(input)
-            File.write(ARGV[1], morse)
