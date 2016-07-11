f46f7edf6ebba44357e0863a1633739621cce8a6
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-07-11 17:47:48 -0600

    Added decoding for multiple words

--- test/decoder_test.rb
+++ test/decoder_test.rb
@@ -1,9 +1,4 @@
-require "simplecov"
-SimpleCov.start
-require "minitest/pride"
-require "minitest/autorun"
-require "./lib/decoder"
-require "./lib/alphabet"
+require_relative 'test_handler'
 
 
 class DecoderTest < Minitest::Test
@@ -11,6 +6,12 @@ class DecoderTest < Minitest::Test
   def test_decode_a_word
     decoder = ParaMorse::Decoder.new
     assert_equal "WORD", decoder.decode("1011101110001110111011100010111010001110101")
+  end
+
+  def test_decodes_multiple_words
+    decoder = ParaMorse::Decoder.new
+    assert_equal "HI THERE", decoder.decode("101010100010100000001110001010101000100010111010001")
+
   end
 
 end
