35754130da54d8f6e34f4e44f1d68911c22d1b37
Raphael Barbo <rsbarbo@gmail.com>
2016-07-11 10:34:03 -0600

    just added some pseaudocoding :octocat:

--- lib/paramorse.rb
+++ lib/paramorse.rb
@@ -1,6 +1,11 @@
 require "pry"
 require_relative "alphabet"
 
+#streamer will receive one character at a time
+#will store in an Array
+#when decode is called, it will either return the word (or letter)
+#or it wil return an error message "it is not a word and will ask another input"
+
 module ParaMorse
 
   class Queue
