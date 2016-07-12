123c6fc62a01fe2609e8e408a9e4bec23ba8abff
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-07-11 18:14:45 -0600

    Added stream_decoder class

--- lib/paramorse.rb
+++ lib/paramorse.rb
@@ -4,6 +4,7 @@ require_relative "decoder"
 require_relative "encoder"
 require_relative "letter_decoder"
 require_relative "letter_encoder"
+require_relative "stream_decoder"
 require_relative "queue"
 
 module ParaMorse
